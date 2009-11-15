5d225e304c6dab5fdd2136f6facb18d82d10e915
Sean Conner <spc@lucy.roswell.conman.org>
2009-11-15 04:00:58 -0500

    New logging script.

---
+++ debug.lua
@@ -0,0 +1,67 @@
+-- ***************************************************************
+--
+-- Copyright 2009 by Sean Conner.  All Rights Reserved.
+-- 
+-- This program is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License
+-- as published by the Free Software Foundation; either version 2
+-- of the License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+--
+-- Comments, questions and criticisms can be sent to: sean@conman.org
+--
+-- ********************************************************************
+
+-- *****************************************************************
+--
+-- Program to print out each field and make sure we're parsing the
+-- logs properly.  This is meant to be used from syslogintr as:
+--
+-- ./syslogintr --ipv4 --ipv6 --local --debug --foreground debug.lua
+--
+-- ******************************************************************
+
+function log(msg)
+  io.stdout:write(string.format("Request:\n"))
+  io.stdout:write(string.format([[
+	version      = %d
+	_RAW         = %s
+	host         = %s
+	hostname     = %s
+	port         = %d
+	remote       = %s
+	timestamp    = %s
+	logtimestamp = %s
+	program      = %s
+	pid          = %d
+	facility     = %s
+	level        = %s
+	msg          = %s
+
+]],
+	msg.version,
+	msg._RAW,
+	msg.host,
+	msg.hostname,
+	msg.port,
+	tostring(msg.remote),
+	os.date("%c",msg.timestamp),
+	os.date("%c",msg.logtimestamp),
+	msg.program,
+	msg.pid,
+	msg.facility,
+	msg.level,
+	msg.msg))
+	
+  io.stdout:flush()
+end
+
+	
