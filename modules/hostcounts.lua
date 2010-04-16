f0c15faa485dfe3c9d7eddcacad6eca0f1a73eb7
spc <spc@conman.org>
2010-04-15 21:40:10 -0400

    Module to count hosts that send us data

---
+++ modules/hostcounts.lua
@@ -0,0 +1,49 @@
+-- ***************************************************************
+--
+-- Copyright 2009 by Sean Conner.  All Rights Reserved.
+-- 
+-- This program is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License
+-- as published by the Free Software Foundation; either version 2
+-- of the License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+--
+-- Comments, questions and criticisms can be sent to: sean@conman.org
+--
+-- ********************************************************************
+
+require "I_log"
+
+if hostcount == nil then
+  hostcount = setmetatable({},{__index = function(t,k) return 0 end })
+end
+
+-- ********************************************************************
+
+function log_hostcounts()
+  local s = ""
+  
+  for name,value in pairs(hostcount) do
+    s = s .. string.format("%s:%d ",name,value)
+    hostcount[name] = 0
+  end
+  
+  I_prlog("summary/hosts","info",s)
+end
+
+-- *********************************************************************
+
+function inc_hostcount(host)
+  hostcount[host] = hostcount[host] + 1
+end
+
+-- ********************************************************************
+
