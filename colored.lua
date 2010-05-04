cd28ffac8f7dae5586168d93bc97ff959da6904b
spc <spc@conman.org>
2010-05-04 05:37:51 -0400

    Fixed copyright statements on code files.

--- colored.lua
+++ colored.lua
@@ -1,3 +1,24 @@
+-- ***************************************************************
+--
+-- Copyright 2010 by Sean Conner.  All Rights Reserved.
+-- 
+-- This program is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License
+-- as published by the Free Software Foundation; either version 2
+-- of the License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+--
+-- Comments, questions and criticisms can be sent to: sean@conman.org
+--
+-- ********************************************************************
 
 
 -- cut -b 1-187 (to account for XTERM escape sequences)
